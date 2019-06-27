211cade07ee0f822c1734c4c1bc9a7fc7fa46b21
PapitomasterDeveloper <papitomasterdeveloper@gmail.com>
2019-06-27 16:37:15 -0500

    Added the before_action hook to run set location by id when calling the jbuilder

---
+++ app/controllers/api/v1/locations_controller.rb
@@ -0,0 +1,19 @@
+class Api::V1::LocationsController < ApplicationController
+	before_action :set_location
+
+	def show
+# The show.json.jbuilder renders a way more appropiate to let know Rails that is a JSON, only on update actions
+# But the code above works almost on the same way, the only difference is that with the jbuilder, Rails figures out all the work to be done in an implicit manner
+# The code above is an explicit manner
+=begin
+		render json: {
+			id: @location.id,
+			name: @location.name
+		}
+=end		
+	end
+
+	def set_location
+		@location = Location.find(params[:id])
+	end
+end
