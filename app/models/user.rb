555f749b1a6dba2319b420905b24c0f885d913f6
PapitomasterDeveloper <papitomasterdeveloper@gmail.com>
2019-06-27 19:36:18 -0500

    Updated the block comments to better understanding

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
+	# Knock makes one assumption about the User model, It must have an authenticate method, using has_secure_password is recommended
 	has_secure_password
 end
